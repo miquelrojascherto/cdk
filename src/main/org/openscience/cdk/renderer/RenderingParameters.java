05904fd91d34e01693c0c8ab1a8af57b4b15b6b3
Egon Willighagen <egon.willighagen@gmail.com>
2010-02-15 17:39:20 +0100

    Converted bondWidth and defaultBondColor into IGeneratorParameter's

--- src/main/org/openscience/cdk/renderer/RenderingParameters.java
+++ src/main/org/openscience/cdk/renderer/RenderingParameters.java
@@ -52,11 +52,6 @@ public class RenderingParameters {
     private double bondLength = 40.0;
 
     /**
-     * The width on screen of a bond
-     */
-    private double bondWidth = 1.0;
-
-    /**
      * The width on screen of an atom-atom mapping line
      */
     private double mappingLineWidth = 1.0;
@@ -71,11 +66,6 @@ public class RenderingParameters {
      * The shape of the compact mode atom.
      */
 //    private AtomShape compactShape = AtomShape.SQUARE;
-
-    /**
-     * The color to draw bonds if not other color is given.
-     */
-    private Color defaultBondColor = Color.BLACK;
 
     private String fontName = "Arial";
 
@@ -217,14 +207,6 @@ public class RenderingParameters {
         this.highlightDistance = highlightDistance;
     }
 
-    public Color getDefaultBondColor() {
-        return defaultBondColor;
-    }
-
-    public void setDefaultBondColor(Color defaultBondColor) {
-        this.defaultBondColor = defaultBondColor;
-    }
-
 	public String getFontName() {
         return this.fontName;
     }
@@ -251,10 +233,6 @@ public class RenderingParameters {
 
     public void setBondLength(double bondLength) {
         this.bondLength = bondLength;
-    }
-
-    public double getBondWidth() {
-        return bondWidth;
     }
 
     public double getMappingLineWidth() {
@@ -331,10 +309,6 @@ public class RenderingParameters {
 
     public void setBondDistance(double bondDistance) {
         this.bondDistance = bondDistance;
-    }
-
-    public void setBondWidth(double bondWidth) {
-        this.bondWidth = bondWidth;
     }
 
     public void setMappingLineWidth(double mappingLineWidth) {
